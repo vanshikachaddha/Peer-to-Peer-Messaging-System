a41c6a20ea2146280cf1f3a182400c67dd73eafa
Vanshika Chaddha <vchaddha@bu.edu>
2025-04-02 01:46:04 -0400

    Updated Files

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 HEADER = 64 
 PORT = 5050
@@ -6,22 +7,49 @@ FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 SERVER = socket.gethostbyname(socket.gethostname()) 
 ADDR = (SERVER, PORT)
+username = None
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
+def start():
+    global username
+    print("Please input your username: ")
+    username = input()
+    while (not username):
+        print("Please input your username: ")
+        username = input()
+    send(username)
+
+
+
 def send(msg):
+
     message = msg.encode(FORMAT) #Encode string into byte like object
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT) #Pad to make 64 bytes
     send_length += b' ' * (HEADER - len(send_length))
     client.send(send_length)
     client.send(message)
-    print(client.recv(2048).decode(FORMAT))
 
-send("Hello World")
-input()
-send("Hello Vanshika")
-input()
-send("Hello Rachel")
-send(DISCONNECT_MESSAGE)
+def recieve():
+    while True:
+        try:
+            message = client.recv(2048).decode(FORMAT)
+            print(message)
+        except:
+            pass
+
+recieve_thread = threading.Thread(target=recieve)
+recieve_thread.start()
+
+start()
+
+while True:
+    message = input()
+    send(message)
+    if message == DISCONNECT_MESSAGE:
+        break
+
+
+
--- server.py
+++ server.py
@@ -7,27 +7,43 @@ SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT) 
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
+clients = {}
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR) #Bound the socket to address
 
 #Runs for each client (running concurrently)
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
+# Conn = connection to client and Addr = address of client (identify which client you're talking to)
+def recieve_message(conn, addr):
     connected = True
+    username = None
 
     while connected:
         #First message tells us how long incoming message is
-        msg_length = conn.recv(HEADER).decode(FORMAT) #Message is encoded in byte format
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
+        try:
+            msg_length = conn.recv(HEADER).decode(FORMAT) #Message is encoded in byte format
+            if msg_length:
+                msg_length = int(msg_length)
+                msg = conn.recv(msg_length).decode(FORMAT)
+                if msg == DISCONNECT_MESSAGE:
+                    connected = False
+                    del clients[addr]
+                
+                if username is None:
+                    username = msg
+                    clients[addr] = (conn, username)
+                    print(f"[NEW CONNECTION] {username} connected.")
+                    continue
 
-            print(f"[{addr}] {msg}")
-      
-            conn.send("Msg recieved".encode(FORMAT))
+                print(f"[{clients[addr][1]}] {msg}")
+
+                
+                for client_addr, (client_conn, name) in clients.items():
+                    if client_addr != addr:
+                        client_conn.send(f"[{name}] {msg}".encode(FORMAT))
+        except:
+            connected = False
+                    
     
     conn.close()
 
@@ -39,7 +55,7 @@ def start():
     while True:
         #Handle new connection
         conn, addr = server.accept() #Wait for new connection of the server (conn - communicate back)
-        thread = threading.Thread(target = handle_client, args = (conn, addr))
+        thread = threading.Thread(target = recieve_message, args = (conn, addr))
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.active_count()-1}")
 
